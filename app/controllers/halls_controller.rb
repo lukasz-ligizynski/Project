ab353901e8c52283afad998593d3e98a00468ee5
Lucas Ligizynski <lukasz.ligizynski@monterail.com>
2021-05-26 13:20:36 +0200

    halls controller

---
+++ app/controllers/halls_controller.rb
@@ -0,0 +1,52 @@
+class HallsController < ApplicationController
+
+    before_action :set_hall, only: [:show, :update, :destroy]
+
+    def index
+        @halls = Hall.all.map do |hall|
+            {
+                name: hall.hall_number,
+                capacity: hall.capacity
+            }
+        end
+        
+        render json: @halls
+    end
+
+    def show
+        @cinema_hall = {
+        id: @hall.id,hall_name: @hall.hall_number, capacity: @hall.capacity,
+        }
+        render json: @hall
+    end
+
+    def create
+        @hall = Hall.new(hall_params)
+        if @hall.save
+            render json: @hall, status: :created
+        else
+            render json: @hall.errors, status: :unprocessable_entity
+        end
+    end
+
+    def update
+        if @hall.update(hall_params)
+            render json: @hall
+        else
+            render json: @hall.errors, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @hall.destroy
+    end
+    
+    private
+    def set_hall
+        @hall = Hall.find(params[:id])
+    end
+        
+    def hall_params
+        params.require(:hall).permit(:hall_number, :capacity)
+    end
+end
