d139304ecae489f1ed476accbeb2862828fa9778
Lucas Ligizynski <lukasz.ligizynski@monterail.com>
2021-05-27 12:08:09 +0200

    Delete Use case

--- app/use_cases/use_case/hall/delete_hall.rb
+++ app/use_cases/use_case/hall/delete_hall.rb
@@ -1,1 +1,28 @@
+module UseCase
+  module Hall
+    class DeleteHall < UseCase::Base
+      def self.call(hall, callbacks, repository = HallRepository.new)
+        new(hall, callbacks, repository).call
+      end
 
+      def call
+        if repository.delete(hall)
+          callbacks[:success].call
+        else
+          callbacks[:failure].call
+        end
+      end
+
+      private
+
+      def initialize(hall, callbacks, repository)
+        super(hall,callbacks, repository)
+        @hall = hall
+        @callbacks = callbacks
+        @repository = repository
+      end
+
+      attr_reader :hall, :repository, :callbacks
+    end
+  end
+end
