9dcfa7c560bb66a9f78fa1083826bc4d4906ef63
Lucas Ligizynski <lukasz.ligizynski@monterail.com>
2021-05-27 09:57:41 +0200

    some repo

---
+++ app/repositories/hall_repository.rb
@@ -0,0 +1,21 @@
+class HallRepository
+  def all
+    Hall.all
+  end
+
+  def find(id)
+    Hall.find(id)
+  end
+
+  def new_entiti(attrs = nill)
+    Hall.new(attrs)
+  end
+
+  def save(hall)
+    hall.save
+  end
+
+  def delete(hall)
+    hall.destroy
+  end
+end
