38e8a1d65794260c4b5125cc29c577c26878c92b
Lucas Ligizynski <lukasz.ligizynski@monterail.com>
2021-05-28 12:43:51 +0200

    Controller and use cases

---
+++ app/concepts/halls/use_cases/delete_hall.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+module Halls
+  module UseCases
+    class Delete
+      attr_reader :repository
+
+      def initialize(repository: Halls::Repository.new)
+          @repository = repository
+      end
+
+      def call(id:)
+          repository.delete(id)
+      end
+    end
+  end
+end
---
+++ app/concepts/halls/use_cases/update_hall.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+module Halls
+  module UseCases
+    class Update
+      attr_reader :repository
+
+      def initialize(repository: Halls::Repository.new)
+        @repository = repository
+      end
+
+      def call(id:, params:)
+        repository.update(id, params)
+      end
+    end
+  end
+end
