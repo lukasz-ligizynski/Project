e2ad50f96b28c9914a49df3cf2aee6e4991d2e4b
Lucas Ligizynski <lukasz.ligizynski@monterail.com>
2021-05-28 11:55:16 +0200

    .

--- config/application.rb
+++ config/application.rb
@@ -37,7 +37,6 @@ module Projekt
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-    config.autoloader = :classic
     config.api_only = true
   end
 end
