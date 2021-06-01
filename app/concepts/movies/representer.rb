c009b31b23a630904eec2fe77c8b315b41ddb663
Lucas Ligizynski <lukasz.ligizynski@monterail.com>
2021-06-01 10:09:32 +0200

    Representer filled

---
+++ app/concepts/movies/representer.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+module Movies
+  class Representer
+    attr_reader :movies
+
+    def initialize(movies)
+      @movies = movies
+    end
+
+    def basic
+      movies.map do |movie|
+        {
+          id: movie.id,
+          title: movie.title,
+          duration: movie.duration,
+          genre: movie.genre,
+          description: movie.description,
+          adult_only: movie.adult_only
+        }
+      end
+    end
+
+    def extended
+      movies.map do |movie|
+        movie
+      end
+    end
+  end
+end
