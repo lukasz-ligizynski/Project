ce537c5b2fb944064998b5f47df4a05eea00f7ca
Lucas Ligizynski <lukasz.ligizynski@monterail.com>
2021-06-15 11:18:17 +0200

    concepts

---
+++ app/concepts/reservations/use_cases/update.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module Reservations
+  module UseCases
+    class Update
+      attr_reader :repository
+
+      def initialize(repository: Reservations::Repository.new)
+        @repository = repository
+      end
+
+      def call(id:, params:)
+        repository.update(id, params)
+      end
+    end
+  end
+end
